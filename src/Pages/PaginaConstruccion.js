e581516d617608955751868c09c70ec9e97eb961
franciscoRoz <francisco.rozas@outlook.com>
2021-05-01 20:43:55 -0400

    correcciones +bootstrap2

--- src/Pages/PaginaConstruccion.js
+++ src/Pages/PaginaConstruccion.js
@@ -13,7 +13,7 @@ export const PaginaConstruccion = () => {
             align="center" 
             className="container">
                 <h1 >página en construcción</h1>
-                <img src="https://i.imgur.com/DsDp9qn.png" alt="imagen de sitio en construccion" height="400px"></img>
+                <img src="https://i.imgur.com/DsDp9qn.png" alt="imagen de sitio en construccion" height="400px" min-height="200px"></img>
                 <br/>
              
                 <p >Pronto podrás comprar todos los productos que quieras.
